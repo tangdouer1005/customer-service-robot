8e0ba2ad6fa8fe1658653acc3bf0c70ed20d8e13
Tangdouer <884746853@qq.com>
2023-11-17 15:10:09 +0800

    在解析函数中增加读取功能

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use crate::parse::script_parse;
+
+
 mod command;
 mod parse;
 mod script;
 
 fn main() {
-    
+    let script_path = "script.txt".to_string();
+
+    #[allow(unused_variables)]
+    let script = script_parse(script_path);
+
+
     println!("Hello, world!");
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Read;
+
 use crate::script::Script;
 
 #[allow(dead_code)]
@@ -12,7 +15,20 @@ impl ParseError{
 }
 
 #[allow(dead_code)]
-pub fn script_parse(src_str: String) -> Result<Script, ParseError>{
+pub fn script_parse(script_path: String) -> Result<Script, ParseError>{
+    let mut file = match File::open(&script_path){
+        Ok(file) => file,
+        Err(s) => panic!("fail to open {}", s),
+    };
+
+    let mut content = String:: new();
+
+    match file.read_to_string(& mut content) {
+        Ok(__) => {
+            println!("read from {}, content: {}", script_path, content)
+        }
+        Err(err) => panic!("fail to read from {}, err: {}", script_path, err),    
+    };
     
     Err(ParseError::new("todo".to_string()))
 }
